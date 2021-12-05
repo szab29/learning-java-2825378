eaad4bb8721dcdc468bb3d5ff27d068aef2a3167
szab29 <szab29@gmail.com>
2021-12-05 16:18:58 -0500

    First changes from IDE.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World! It's me, Szab! Hihi!");
+		System.out.println("Hello World! It's me, Szab! What's up?");
 	}
 }
